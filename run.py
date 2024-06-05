1f6a74b361ce759ec614112267c386b1f7a77f5a
Milena Mihajilovic <ema.mihajilovic@gmail.com>
2024-06-05 14:37:34 +0000

    Create the create_board function

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 
 
 def print_board(board):
-    """ Creating the board.
-        Prints the board in a readable format.
+    """ 
+    Creating the board.
+    Prints the board in a readable format.
     """
     for row in board: 
         print(" ".join(row))
 
+def create_board(board):
+    """
+    Creates a size x size board filled with "O", which represent the sea.
+    """
+    board = [["O"] * size for _ in range(size)]
+    return board
