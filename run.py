b1189f34fd7984066bf306ea6fd7c80026e72166
Milena Mihajilovic <ema.mihajilovic@gmail.com>
2024-06-05 15:29:02 +0000

    Create make_guess function and use the int() function for integer numbers.

--- run.py
+++ run.py
@@ -22,3 +22,13 @@ def set_ship(board, size):
     ship_row = random.randint(0, size -1)
     ship_col = random.randint(0, size -1)
     return (ship_row, ship_col)
+
+def make_guess ():
+    """
+    Prompts the player to input their guess for the row and column.
+    """
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
+    return(guess_row, guess_col)
+
+    
