c54aa3b7d7f032e3ea1999eec918528e56f248ae
Milena Mihajilovic <ema.mihajilovic@gmail.com>
2024-06-05 14:21:36 +0000

    Create the print_board function

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def print_board(board):
+    """ Creating the board.
+        Prints the board in a readable format.
+    """
+
+    for row in board: 
+        print(" ".join(row))
+
